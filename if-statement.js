f734be52e9a2014c5ea1d42e442e62103caad3e4
Saulo Pimenta <sebbsp@gmail.com>
2020-09-10 05:27:01 +0000

    Commit of the JS workshop

---
+++ if-statement.js
@@ -0,0 +1,7 @@
+const fruit = 'orange';
+
+if (fruit.length > 5) {
+  console.log('The fruit name has more than five characters.');
+} else {
+  console.log('THe fuit name has five characters or less.');
+}
